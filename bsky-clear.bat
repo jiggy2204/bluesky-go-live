10684f4df1e8762f3b227b307eadb3d0b450354d
Jiggy <jignacio2204@gmail.com>
2026-03-09 13:51:19 -0400

    Create bsky-clear.bat for clearing stream status
    Add batch script to clear stream status.

---
+++ bsky-clear.bat
@@ -0,0 +1,4 @@
+:: bsky-clear.bat
+:: Fires when your stream ends. Same paths as above, just "clear" instead of "set".
+@echo off
+"C:\Program Files\nodejs\node.exe" "C:\Users\YourName\path\to\bsky-live-status.js" clear
